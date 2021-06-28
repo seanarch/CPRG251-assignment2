37846c9442906cb7b3a67fa6997cad43c24732f8
Cate Huang <s030228@gmail.com>
2021-06-28 15:54:03 -0600

    Part 1 – Non-functional front-end GUI.

--- src/sait/frms/gui/FlightsTab.java
+++ src/sait/frms/gui/FlightsTab.java
@@ -2,6 +2,7 @@ package sait.frms.gui;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.*;
 
 import sait.frms.manager.FlightManager;
@@ -46,13 +47,160 @@ public class FlightsTab extends TabBase
 		
 		panel.setLayout(new BorderLayout());
 		
-		JPanel northPanel = createNorthPanel();
-		panel.add(northPanel, BorderLayout.NORTH);
-		
-		JPanel centerPanel = createCenterPanel();
-		panel.add(centerPanel, BorderLayout.CENTER);
+		panel.add(createNorthPanel(), BorderLayout.NORTH);
+		panel.add(createCenterPanel(), BorderLayout.CENTER);
+		panel.add(createSouthPanel(), BorderLayout.SOUTH);
+		panel.add(createEastPanel(), BorderLayout.EAST);
 	}
 	
+	private JPanel createSouthPanel() {
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		
+		JLabel labelTitle = new JLabel("Flight Finder", SwingConstants.CENTER);
+		labelTitle.setFont(new Font("serif", Font.PLAIN, 29));
+		panel.add(labelTitle, BorderLayout.NORTH);
+		
+		JPanel gridbag = new JPanel();
+		gridbag.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		JLabel labelFrom = new JLabel("From:");
+		c.gridx = 0;
+		c.gridy = 0;
+		gridbag.add(labelFrom, c);
+		
+		JComboBox comboboxFrom = new JComboBox();
+		c.gridx = 1;
+		c.gridy = 0;
+		gridbag.add(comboboxFrom, c);
+		
+		JLabel labelTo = new JLabel("To:");
+		c.gridx = 0;
+		c.gridy = 1;
+		gridbag.add(labelTo, c);
+		
+		JComboBox comboboxTo = new JComboBox();
+		c.gridx = 1;
+		c.gridy = 1;
+		gridbag.add(comboboxTo, c);
+		
+		JLabel labelDay = new JLabel("Day:");
+		c.gridx = 0;
+		c.gridy = 2;
+		gridbag.add(labelDay, c);
+		
+		JComboBox comboboxDay = new JComboBox();
+		c.gridx = 1;
+		c.gridy = 2;
+		gridbag.add(comboboxDay, c);
+		
+		panel.add(gridbag, BorderLayout.CENTER);
+		
+		panel.add(new JButton("Find Flights"), BorderLayout.SOUTH);
+		
+		return panel;
+	}
+
+	private JPanel createEastPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 0));
+		
+		JLabel labelTitle = new JLabel("Reserve", SwingConstants.CENTER);
+		labelTitle.setFont(new Font("serif", Font.PLAIN, 29));
+		panel.add(labelTitle, BorderLayout.NORTH);
+		
+		JPanel gridbag = new JPanel();
+		gridbag.setLayout(new GridBagLayout());
+		gridbag.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 0));
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		JLabel labelFlight = new JLabel("Flight:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 0;
+		gridbag.add(labelFlight, c);
+		
+		TextField textFlight = new TextField(10);
+		textFlight.setEditable(false);
+		c.gridx = 1;
+		c.gridy = 0;
+		gridbag.add(textFlight, c);
+		
+		JLabel labelAirline = new JLabel("Airline:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 1;
+		gridbag.add(labelAirline, c);
+		
+		TextField textAirline = new TextField(10);
+		textAirline.setEditable(false);
+		c.gridx = 1;
+		c.gridy = 1;
+		gridbag.add(textAirline, c);
+		
+		JLabel labelDay = new JLabel("Day:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 2;
+		gridbag.add(labelDay, c);
+		
+		TextField textDay = new TextField(10);
+		textDay.setEditable(false);
+		c.gridx = 1;
+		c.gridy = 2;
+		gridbag.add(textDay, c);
+		
+		JLabel labelTime = new JLabel("Time:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 3;
+		gridbag.add(labelTime, c);
+		
+		TextField textTime = new TextField(10);
+		textTime.setEditable(false);
+		c.gridx = 1;
+		c.gridy = 3;
+		gridbag.add(textTime, c);
+
+		JLabel labelCost = new JLabel("Cost:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 4;
+		gridbag.add(labelCost, c);
+		
+		TextField textCost = new TextField(10);
+		textCost.setEditable(false);
+		c.gridx = 1;
+		c.gridy = 4;
+		gridbag.add(textCost, c);
+		
+		JLabel labelName = new JLabel("Name:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 5;
+		gridbag.add(labelName, c);
+		
+		TextField textName = new TextField(10);
+		c.gridx = 1;
+		c.gridy = 5;
+		gridbag.add(textName, c);
+		
+		JLabel labelCitizenship = new JLabel("Citizenship:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 6;
+		gridbag.add(labelCitizenship, c);
+		
+		TextField textCitizenship = new TextField();
+		c.gridx = 1;
+		c.gridy = 6;
+		gridbag.add(textCitizenship, c);
+		
+		panel.add(gridbag, BorderLayout.CENTER);
+		
+		panel.add(new JButton("Reserve"), BorderLayout.SOUTH);
+
+		return panel;
+	}
+
 	/**
 	 * Creates the north panel.
 	 * @return JPanel that goes in north.
@@ -75,8 +223,8 @@ public class FlightsTab extends TabBase
 	private JPanel createCenterPanel() 
 	{
 		JPanel panel = new JPanel();
-		
 		panel.setLayout(new BorderLayout());
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 10));
 		
 		flightsModel = new DefaultListModel<>();
 		flightsList = new JList<>(flightsModel);
--- src/sait/frms/gui/ReservationsTab.java
+++ src/sait/frms/gui/ReservationsTab.java
@@ -3,6 +3,8 @@ package sait.frms.gui;
 import java.awt.*;
 import javax.swing.*;
 
+//import sait.frms.gui.FlightsTab.MyListSelectionListener;
+//import sait.frms.gui.FlightsTab.MyListSelectionListener;
 import sait.frms.manager.ReservationManager;
 import sait.frms.problemdomain.Reservation;
 
@@ -18,6 +20,153 @@ public class ReservationsTab extends TabBase {
 	
 	private JList<Reservation> reservationsList;
 	
+	private JPanel createSouthPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		
+		JLabel labelTitle = new JLabel("Search", SwingConstants.CENTER);
+		labelTitle.setFont(new Font("serif", Font.PLAIN, 29));
+		panel.add(labelTitle, BorderLayout.NORTH);
+		
+		JPanel gridbag = new JPanel();
+		gridbag.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		JLabel labelCode = new JLabel("Code:");
+		c.gridx = 0;
+		c.gridy = 0;
+		gridbag.add(labelCode, c);
+		
+		JTextField textCode = new JTextField(50);
+		c.gridx = 1;
+		c.gridy = 0;
+		gridbag.add(textCode, c);
+		
+		JLabel labelAirline = new JLabel("Airline:");
+		c.gridx = 0;
+		c.gridy = 1;
+		gridbag.add(labelAirline, c);
+		
+		JTextField textAirline = new JTextField(50);
+		c.gridx = 1;
+		c.gridy = 1;
+		gridbag.add(textAirline, c);
+		
+		JLabel labelName = new JLabel("Name:");
+		c.gridx = 0;
+		c.gridy = 2;
+		gridbag.add(labelName, c);
+		
+		JTextField textName = new JTextField(50);
+		c.gridx = 1;
+		c.gridy = 2;
+		gridbag.add(textName, c);
+		
+		panel.add(gridbag, BorderLayout.CENTER);
+		
+		panel.add(new JButton("Find Reservations"), BorderLayout.SOUTH);
+
+		return panel;
+	}
+
+	private JPanel createEastPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 0));
+		
+		JLabel labelTitle = new JLabel("Reserve", SwingConstants.CENTER);
+		labelTitle.setFont(new Font("serif", Font.PLAIN, 29));
+		panel.add(labelTitle, BorderLayout.NORTH);
+		
+		JPanel gridbag = new JPanel();
+		gridbag.setLayout(new GridBagLayout());
+		gridbag.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 0));
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		JLabel labelCode = new JLabel("Code:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 0;
+		gridbag.add(labelCode, c);
+		
+		TextField textFlight = new TextField(10);
+		textFlight.setEditable(false);
+		c.gridx = 1;
+		c.gridy = 0;
+		gridbag.add(textFlight, c);
+		
+		JLabel labelFlight = new JLabel("Flight:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 1;
+		gridbag.add(labelFlight, c);
+		
+		TextField textAirline = new TextField(10);
+		textAirline.setEditable(false);
+		c.gridx = 1;
+		c.gridy = 1;
+		gridbag.add(textAirline, c);
+		
+		JLabel labelAirline = new JLabel("Airline:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 2;
+		gridbag.add(labelAirline, c);
+		
+		TextField textDay = new TextField(10);
+		textDay.setEditable(false);
+		c.gridx = 1;
+		c.gridy = 2;
+		gridbag.add(textDay, c);
+		
+		JLabel labelCost = new JLabel("Cost:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 3;
+		gridbag.add(labelCost, c);
+		
+		TextField textTime = new TextField(10);
+		textTime.setEditable(false);
+		c.gridx = 1;
+		c.gridy = 3;
+		gridbag.add(textTime, c);
+
+		JLabel labelName = new JLabel("Name:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 4;
+		gridbag.add(labelName, c);
+		
+		TextField textCost = new TextField(10);
+		c.gridx = 1;
+		c.gridy = 4;
+		gridbag.add(textCost, c);
+		
+		JLabel labelCitizenship = new JLabel("Citizenship:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 5;
+		gridbag.add(labelCitizenship, c);
+		
+		TextField textName = new TextField(10);
+		c.gridx = 1;
+		c.gridy = 5;
+		gridbag.add(textName, c);
+		
+		JLabel labelStatus= new JLabel("Status:", SwingConstants.RIGHT);
+		c.gridx = 0;
+		c.gridy = 6;
+		gridbag.add(labelStatus, c);
+		
+		JComboBox textStatus = new JComboBox();
+		c.gridx = 1;
+		c.gridy = 6;
+		gridbag.add(textStatus, c);
+		
+		panel.add(gridbag, BorderLayout.CENTER);
+		
+		panel.add(new JButton("Update"), BorderLayout.SOUTH);
+
+		return panel;
+	}
+	
+	
 	/**
 	 * Creates the components for reservations tab.
 	 */
@@ -27,7 +176,28 @@ public class ReservationsTab extends TabBase {
 		
 		JPanel northPanel = createNorthPanel();
 		panel.add(northPanel, BorderLayout.NORTH);
+		
+		JPanel centerPanel = createCenterPanel();
+		panel.add(centerPanel, BorderLayout.CENTER);
+		
+		JPanel southPanel = createSouthPanel();
+		panel.add(southPanel, BorderLayout.SOUTH);
+		
+		JPanel eastPanel = createEastPanel();
+		panel.add(eastPanel, BorderLayout.EAST);
+		
 	}
+	
+	private JPanel createCenterPanel() 
+	{
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 10));
+		panel.add(new JTextArea());
+
+		return panel;
+	}
+	
 	
 	/**
 	 * Creates the north panel.
@@ -43,4 +213,6 @@ public class ReservationsTab extends TabBase {
 		
 		return panel;
 	}
+	
+	
 }
