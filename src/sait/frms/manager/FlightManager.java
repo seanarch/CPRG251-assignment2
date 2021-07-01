d6b94acfc3f32dd32d382febe3a4bcf47d263bb7
seanzhu <zhuqingyun@outlook.com>
2021-07-01 13:59:48 +0800

    Added function getFlight

--- src/sait/frms/manager/FlightManager.java
+++ src/sait/frms/manager/FlightManager.java
@@ -1,8 +1,11 @@
-package sait.frms.manager;
+package src.sait.frms.manager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-import sait.frms.problemdomain.*;
+import src.sait.frms.problemdomain.*;
 
 public class FlightManager {
 	static final String WEEKDAY_ANY;
@@ -15,6 +18,7 @@ public class FlightManager {
 	static final String WEEKDAY_SATURDAY;
 	private ArrayList<Flight> flights;
 	private ArrayList<String> airports;
+	private final String FILE_PATH = "res/flights.csv";
 	
 	
 	public FlightManager() {
@@ -22,7 +26,29 @@ public class FlightManager {
 		// TODO Auto-generated constructor stub
 	}
 
-	public ArrayList<Flight> getFlights() {
+	public ArrayList<Flight> getFlights() throws FileNotFoundException {
+		Scanner in = new Scanner(new File(FILE_PATH));
+
+		while (in.hasNext()) {
+			String[] fields = in.nextLine().split(",");
+
+			//need to figure out what does the code refer to, so use placeholder first
+			//String code, String airlineName, String from, String to, String weekday, String time, int seats,
+			//double costPerSeat
+			String code = "Placeholder";
+
+			String airlineName = fields[0];
+			String from = fields[1];
+			String to = fields[2];
+			String weekday = fields[3];
+			String time = fields[4];
+			int seats = Integer.parseInt(fields[5]);
+			double costPerSeat = Double.parseDouble(fields[6]);
+
+			flights.add(new Flight(code, airlineName, from, to, weekday, time, seats, costPerSeat));
+
+		}
+		in.close();
 		return flights;
 	}
 
